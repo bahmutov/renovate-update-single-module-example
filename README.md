b46bb403bf876139b80e43e354c8e08b1e81dc07
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-31 12:31:05 -0400

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# renovate-update-single-module-example
+> Example repository where only a single dependency is updated using Renovate Bot
---
+++ index.js
@@ -0,0 +1,5 @@
+// print ALL debug messages
+const debug = require('debug')('*')
+const chalk = require('chalk')
+debug('it works')
+console.log(chalk.green('chalk is working'))
